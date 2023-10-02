751e6688f641419e37dfe7f6ab9b311d2af9ac0d
A-Zhelyazkov <146733355+A-Zhelyazkov@users.noreply.github.com>
2023-10-02 15:57:23 +0300

    Delete nested_loops directory

--- nested_loops/clock.py
+++
@@ -1,3 +0,0 @@
-for i in range(24):
-    for k in range(60):
-        print(f"{i}:{k}")
--- nested_loops/multiplication_table.py
+++
@@ -1,4 +0,0 @@
-for a in range(1, 11):
-    for b in range(1, 11):
-        results = a * b
-        print(f"{a} * {b} = {results}")
