9b930e99eb6ce4840a50ef3fea418facd7c82806
A-Zhelyazkov <146733355+A-Zhelyazkov@users.noreply.github.com>
2023-10-08 21:28:52 +0300

    Add files via upload
    updated 04, 08 and 10 with correct fk names to match 100/100

--- PostgreSQL/table_relations/exercise/08_study_session.sql
+++ PostgreSQL/table_relations/exercise/08_study_session.sql
@@ -1,68 +1,69 @@
-CREATE TABLE students(
+CREATE TABLE students (
 	id SERIAL PRIMARY KEY,
-	student_name VARCHAR(30)
+	student_name VARCHAR(50)
 );
 
-CREATE TABLE exams(
-	id INT GENERATED ALWAYS AS IDENTITY (START WITH 101 INCREMENT 1) PRIMARY KEY,
-	exam_name VARCHAR(30)
+CREATE TABLE exams (
+	id INT GENERATED ALWAYS AS IDENTITY (START 101 INCREMENT 1) PRIMARY KEY,
+	exam_name VARCHAR(50)
 );
 
-CREATE TABLE study_halls(
+CREATE TABLE study_halls (
 	id SERIAL PRIMARY KEY,
-	study_hall_name VARCHAR(30),
+	study_hall_name VARCHAR(50),
 	exam_id INT,
 
 	CONSTRAINT fk_study_halls_exams
-		FOREIGN KEY (exam_id)
-			REFERENCES exams(id)
+	FOREIGN KEY (exam_id)
+	REFERENCES exams(id)
 );
 
-CREATE TABLE students_exams(
+CREATE TABLE students_exams (
 	student_id INT,
 	exam_id INT,
 
 	CONSTRAINT pk_students_exams
 	PRIMARY KEY (student_id, exam_id),
 
-	CONSTRAINT fk_students_exams_exams
-		FOREIGN KEY (exam_id)
-			REFERENCES exams(id),
-
 	CONSTRAINT fk_students_exams_students
-		FOREIGN KEY (student_id)
-			REFERENCES students(id)
+	FOREIGN KEY (student_id)
+	REFERENCES students(id),
 
+	CONSTRAINT fk_students_exams_exams
+	FOREIGN KEY (exam_id)
+	REFERENCES exams(id)
 );
 
-INSERT INTO students (student_name)
+
+INSERT INTO
+	students(student_name)
 VALUES
 	('Mila'),
 	('Toni'),
-	('Ron')
-;
+	('Ron');
 
-INSERT INTO exams(exam_name)
+INSERT INTO
+	exams(exam_name)
 VALUES
 	('Python Advanced'),
 	('Python OOP'),
-	('PostgreSQL')
-;
+	('PostgreSQL');
 
-INSERT INTO study_halls(study_hall_name, exam_id)
+INSERT INTO
+	study_halls(study_hall_name, exam_id)
 VALUES
 	('Open Source Hall', 102),
 	('Inspiration Hall', 101),
 	('Creative Hall', 103),
 	('Masterclass Hall', 103),
-	('Information Security Hall', 103)
-;
+	('Information Security Hall', 103);;
 
-INSERT INTO students_exams (student_id, exam_id)
+INSERT INTO
+	students_exams
 VALUES
 	(1, 101),
 	(1, 102),
 	(2, 101),
 	(3, 103),
 	(2, 102),
-	(2, 103);
+	(2, 103);;
--- PostgreSQL/table_relations/exercise/10_online_store.sql
+++ PostgreSQL/table_relations/exercise/10_online_store.sql
@@ -1,54 +1,56 @@
-CREATE TABLE item_types(
+CREATE TABLE item_types (
 	id SERIAL PRIMARY KEY,
-	item_type_name VARCHAR(30)
+	item_type_name VARCHAR(50)
 );
 
-CREATE TABLE items(
+CREATE TABLE cities (
 	id SERIAL PRIMARY KEY,
-	item_name VARCHAR(30),
+	city_name VARCHAR(50)
+);
+
+CREATE TABLE items (
+	id SERIAL PRIMARY KEY,
+	item_name VARCHAR(40),
 	item_type_id INT,
 
 	CONSTRAINT fk_items_item_types
-		FOREIGN KEY (item_type_id)
-			REFERENCES item_types(id)
+	FOREIGN KEY (item_type_id)
+	REFERENCES item_types(id)
 );
 
-CREATE TABLE cities(
+CREATE TABLE customers (
 	id SERIAL PRIMARY KEY,
-	city_name VARCHAR(30)
-);
-
-CREATE TABLE customers(
-	id SERIAL PRIMARY KEY,
-	customer_name VARCHAR(30),
+	customer_name VARCHAR(100),
 	birthday DATE,
 	city_id INT,
 
 	CONSTRAINT fk_customers_cities
-		FOREIGN KEY (city_id)
-			REFERENCES cities(id)
+	FOREIGN KEY (city_id)
+	REFERENCES cities(id)
 );
 
-CREATE TABLE orders(
+CREATE TABLE orders (
 	id SERIAL PRIMARY KEY,
 	customer_id INT,
+
 	CONSTRAINT fk_orders_customers
-		FOREIGN KEY (customer_id)
-			REFERENCES customers(id)
+	FOREIGN KEY (customer_id)
+	REFERENCES customers(id)
 );
 
-CREATE TABLE order_items(
+
+CREATE TABLE order_items (
 	order_id INT,
 	item_id INT,
 
 	CONSTRAINT pk_order_items
 	PRIMARY KEY (order_id, item_id),
 
-	CONSTRAINT order_items_items
-		FOREIGN KEY (item_id)
-			REFERENCES items(id),
+	CONSTRAINT fk_order_items_orders
+	FOREIGN KEY (order_id)
+	REFERENCES orders(id),
 
-	CONSTRAINT order_items_orders
-		FOREIGN KEY (order_id)
-			REFERENCES orders(id)
-)
+	CONSTRAINT fk_order_items_items
+	FOREIGN KEY (item_id)
+	REFERENCES items(id)
+);
