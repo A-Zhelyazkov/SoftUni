32fcf3ea716416a23e116198fbbccd138ff59305
A-Zhelyazkov <146733355+A-Zhelyazkov@users.noreply.github.com>
2023-11-26 02:25:58 +0200

    adding exercise

---
+++ ORM/advanced_model_techniques/exercise/main_app/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_name(value):
+    for char in value:
+        if not (char.isalpha() or char.isspace()):
+            raise ValidationError("Name can only contain letters and spaces")
+
+
+def validate_phone(value):
+    if value[:4] != "+359" or len(value) != 13:
+        raise ValidationError("Phone number must start with a '+359' followed by 9 digits")
